c3b716d96bbcb160b3a9b5eb82a330be2a0c1ba1
yuuu <okazaki@fusic.co.jp>
2025-02-03 05:48:24 +0900

    Add FAT FileSystem on ESP32

--- mrblib/main_task.rb
+++ mrblib/main_task.rb
@@ -10,12 +10,31 @@ begin
     STDIN.echo = false
     $shell = Shell.new(clean: true)
 
+    puts "Initializing FLASH disk as the root volume... "
+    $shell.setup_root_volume(:flash, label: 'storage')
+    $shell.setup_system_files
+    puts "Available"
+  end
+rescue => e
+  puts "Not available"
+  puts "#{e.message} (#{e.class})"
+end
+
+begin
+  Machine.using_delay do
+    if File.exist?("/home/app.mrb")
+      puts "Loading app.mrb"
+      load "/home/app.mrb"
+    elsif File.exist?("/home/app.rb")
+      puts "Loading app.rb"
+      load "/home/app.rb"
+    end
+
     puts "Starting shell...\n\n"
     
     $shell.show_logo
     $shell.start
   end
 rescue => e
-  puts "Not available"
   puts "#{e.message} (#{e.class})"
 end
